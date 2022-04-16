17d957833d8f3bb16679d418d0e0d964b4635219
Ethan Myos <myos0004@umn.edu>
2022-04-16 17:24:45 -0500

    adding toggle writing functionality

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 // size of grid is 560x560px
+let isWriting = false;
 
 const grid = document.querySelector('.grid');
 const clearGrid = document.querySelector('.clear');
 const changeGridSize = document.querySelector('.changeSize');
 
-// start original 16x16 grid
-for(let i=0; i<256; i++) {
-        const d = document.createElement('div');
-        d.style.width = '35px'; // 560px divided by 16 rows is 35px
-        d.style.height = '35px';
-        d.style.backgroundColor = 'white'; // set background color to white to have contrast
-        d.addEventListener("mouseover", () => d.style.backgroundColor = 'black');
-        grid.appendChild(d);
-}
 // add event listeners to clear the grid when this button is clicked
 // and to change the grid size to the desired size
-clearGrid.addEventListener("click", () => gridClearing(grid));
-changeGridSize.addEventListener("click", () => changeSize(grid));
+clearGrid.addEventListener('click', () => gridClearing());
+changeGridSize.addEventListener('click', () => changeSize(grid, 0));
+// event listener to toggle writing when grid is clicked on
+grid.addEventListener('click', () => writing(grid));
+
+function writing(grid) {
+    let children = grid.children
+    if(isWriting) { // toggle writing off and remove event listener from all cells in grid
+        isWriting = false;
+        for(let i=0; i<children.length; i++) {
+            children[i].removeEventListener('mouseleave', write);
+        }
+    }
+    else { // toggle writing on and add event listener to all cells in grid
+        isWriting = true
+        for(let i=0; i<children.length; i++) {
+            children[i].addEventListener('mouseleave', write);
+        }
+    }
+}
+
+function write(c) { // function to make background color black when the mouse leaves the cell
+    c.target.style.backgroundColor = 'black';
+}
+
 
 // sets the background color of each cell in the grid to white, clearing the grid
-function gridClearing(grid) {
+function gridClearing() {
     let children = grid.children;
     for(let i=0; i<children.length; i++) {
         children[i].style.backgroundColor = 'white';
@@ -29,8 +44,7 @@ function gridClearing(grid) {
 // asks the user for the size of the grid, decide the size in px of each cell, clear the grid, 
 // remove all children from the grid container div in the HTML, then add all of the new cells to the grid, 
 // initializing the background color and having the correct size
-function changeSize(grid) {
-    let dimension = 0;
+function changeSize(grid, dimension) {
     while(dimension < 16 || dimension > 100) { // have the user select a dimension between 16x16 and 100x100
         dimension = parseInt(prompt("How many rows do you want? (between 16 and 100) "));
     }
@@ -43,7 +57,7 @@ function changeSize(grid) {
         d.style.width = `${size}px`;
         d.style.height = `${size}px`;
         d.style.backgroundColor = 'white';
-        d.addEventListener("mouseover", () => d.style.backgroundColor = 'black');
+        //d.addEventListener("mouseover", () => d.style.backgroundColor = 'black');
         grid.appendChild(d);
     }
 }
@@ -53,3 +67,5 @@ function removeAllChildNodes(parent) {
         parent.removeChild(parent.firstChild);
     }
 }
+// initialize grid of 16x16
+changeSize(grid, 16);
